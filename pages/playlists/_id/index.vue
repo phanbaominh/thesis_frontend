e9963310b4043bcc7a1b657c573147add8282bd8
Phan Bao Minh <pbminh2511@gmail.com>
2021-04-26 16:51:16 +0700

    Minor fix

--- pages/playlists/_id/index.vue
+++ pages/playlists/_id/index.vue
@@ -20,13 +20,9 @@ import Vue from 'vue';
 import { Playlist } from 'types/types';
 export default Vue.extend({
   async asyncData({ route, $axios, $apiUrl }) {
-    try {
-      const playlist = (await $axios.$get($apiUrl.playlist(route.params.id)))
-        .playlists;
-      return { playlist };
-    } catch {
-      // DO NOTHING
-    }
+    const playlist = (await $axios.$get($apiUrl.playlist(route.params.id)))
+      .playlists;
+    return { playlist };
   },
   data() {
     return {
